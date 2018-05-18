8f205f1c4d64274977f8542368c9860f78f29d9c
Harsha Srinivas <33371113+srinivasharsha@users.noreply.github.com>
2018-05-18 23:28:48 +0530

    added test file

--- dashboard/partials/button/test.php
+++ dashboard/partials/button/test.php
@@ -4,4 +4,5 @@ $id=3;
 $result = mysqli_query($con, "SELECT distinct devicecredentials.resource,devicecredentials.auth from devicecredentials WHERE devicecredentials.Dev_Id=$id" ) or die("Query fail :" .mysqli_error());
 	$row = mysqli_fetch_array($result);
 	echo $row['resource'];
-	?>
+	
+?>
